b7af936c83b0877d00c053355c003e969aa40862
Kyle Scott <kyscott@umich.edu>
2022-03-29 15:15:06 -0400

    query next positionID

--- src/fetchAllSwaps.ts
+++ src/fetchAllSwaps.ts
@@ -1,156 +1,221 @@
-import { SignerWallet, SolanaProvider } from "@saberhq/solana-contrib";
-import { deserializeMint } from "@saberhq/token-utils";
-import type { SwapInfoData } from "@senchahq/sencha-sdk";
-import { SenchaSDK } from "@senchahq/sencha-sdk";
+import { getAddress } from "@ethersproject/address";
+import { AddressZero } from "@ethersproject/constants";
+import type { ContractInterface } from "@ethersproject/contracts";
+import { Contract } from "@ethersproject/contracts";
+import type { JsonRpcProvider } from "@ethersproject/providers";
+import { StaticJsonRpcProvider } from "@ethersproject/providers";
 import { StaticTokenListResolutionStrategy } from "@solana/spl-token-registry";
-import type { AccountInfo, PublicKey } from "@solana/web3.js";
-import { Connection, Keypair } from "@solana/web3.js";
 import * as fs from "fs/promises";
-import { chunk, zip } from "lodash";
+
+import HOMORA_BANK_ABI from "./abis/HomoraBank.json";
+import type { HomoraBank } from "./generated";
 
 const TOKEN_LIST = new StaticTokenListResolutionStrategy().resolve();
 
 const MAX_CHUNK = 100;
 
+// returns the checksummed address if the address is valid, otherwise returns false
+export function isAddress(value: string): string | false {
+  try {
+    return getAddress(value);
+  } catch {
+    return false;
+  }
+}
+
+// account is optional
+export function getContract(
+  address: string,
+  ABI: ContractInterface,
+  provider: JsonRpcProvider
+): Contract {
+  if (!isAddress(address) || address === AddressZero) {
+    throw Error(`Invalid 'address' parameter '${address}'.`);
+  }
+  return new Contract(address, ABI, provider);
+}
+
+function useContract(
+  address: string | undefined,
+  ABI: ContractInterface,
+  provider: JsonRpcProvider
+): Contract | null {
+  if (!address || !ABI) return null;
+  try {
+    return getContract(address, ABI, provider);
+  } catch (error) {
+    console.error("Failed to get contract", error);
+    return null;
+  }
+}
+
+function useBankContract(provider: JsonRpcProvider): HomoraBank | null {
+  return useContract(
+    "0x827cCeA3D460D458393EEAfE831698d83FE47BA7",
+    HOMORA_BANK_ABI.abi,
+    provider
+  ) as HomoraBank | null;
+}
+
+export interface Positions {
+  nextPositionID: string;
+  ownerMap: {
+    positionID: string;
+    owner: string;
+  }[];
+}
+
 export const fetchAllSwaps = async (): Promise<void> => {
-  const provider = SolanaProvider.load({
-    connection: new Connection("https://sencha.rpcpool.com"),
-    wallet: new SignerWallet(Keypair.generate()),
-  });
+  const provider = new StaticJsonRpcProvider("https://forno.celo.org");
 
-  const getMultipleAccountsInfoChunked = async (keys: PublicKey[]) => {
-    const keyChunks = chunk(keys, MAX_CHUNK);
-    return (
-      await Promise.all(
-        keyChunks.map(async (c) => {
-          return await sencha.provider.connection.getMultipleAccountsInfo(c);
-        })
-      )
-    ).flat();
+  const bankContract = useBankContract(provider);
+
+  const nextPositionID = await bankContract?.nextPositionId();
+
+  const positions: Positions = {
+    nextPositionID: nextPositionID?.toString() ?? "1",
+    ownerMap: [],
   };
 
-  const sencha = SenchaSDK.load({ provider });
-  const metas = await sencha.loadFactory().fetchAllSwapMetas();
-  const swaps = await getMultipleAccountsInfoChunked(metas.map((m) => m.swap));
+  await fs.writeFile("data/positions.json", JSON.stringify(positions, null, 2));
+  // const provider = SolanaProvider.load({
+  //   connection: new Connection("https://sencha.rpcpool.com"),
+  //   wallet: new SignerWallet(Keypair.generate()),
+  // });
 
-  const swapInfos = swaps
-    .filter((s): s is AccountInfo<Buffer> => !!s)
-    .map((s) =>
-      sencha.programs.CpAmm.coder.accounts.decode<SwapInfoData>(
-        "SwapInfo",
-        s.data
-      )
-    );
+  // const getMultipleAccountsInfoChunked = async (keys: PublicKey[]) => {
+  //   const keyChunks = chunk(keys, MAX_CHUNK);
+  //   return (
+  //     await Promise.all(
+  //       keyChunks.map(async (c) => {
+  //         return await sencha.provider.connection.getMultipleAccountsInfo(c);
+  //       })
+  //     )
+  //   ).flat();
+  // };
 
-  const mintKeys = swapInfos.map((swap) => swap.poolMint);
-  const mints = zip(
-    mintKeys,
-    (await getMultipleAccountsInfoChunked(mintKeys)).map((s) =>
-      s ? deserializeMint(s.data) : null
-    )
-  );
+  // const sencha = SenchaSDK.load({ provider });
+  // const metas = await sencha.loadFactory().fetchAllSwapMetas();
+  // const swaps = await getMultipleAccountsInfoChunked(metas.map((m) => m.swap));
 
-  const accounts: { coingeckoId: string; account: PublicKey }[] = [];
-  const unknownAccounts: { mint: PublicKey; account: PublicKey }[] = [];
+  // const swapInfos = swaps
+  //   .filter((s): s is AccountInfo<Buffer> => !!s)
+  //   .map((s) =>
+  //     sencha.programs.CpAmm.coder.accounts.decode<SwapInfoData>(
+  //       "SwapInfo",
+  //       s.data
+  //     )
+  //   );
 
-  const pushToken = ({
-    coingeckoId,
-    mint,
-    reserves,
-  }: {
-    coingeckoId?: string;
-    mint: PublicKey;
-    reserves: PublicKey;
-  }) => {
-    if (!coingeckoId) {
-      // treat CASH as USDC for accounting purposes
-      if (mint.toString() === "CASHVDm2wsJXfhj6VWxb7GiMdoLc17Du7paH4bNr5woT") {
-        coingeckoId = "usd-coin";
-      }
-    }
-    if (coingeckoId) {
-      accounts.push({
-        coingeckoId,
-        account: reserves,
-      });
-    } else {
-      unknownAccounts.push({
-        mint,
-        account: reserves,
-      });
-    }
-  };
+  // const mintKeys = swapInfos.map((swap) => swap.poolMint);
+  // const mints = zip(
+  //   mintKeys,
+  //   (await getMultipleAccountsInfoChunked(mintKeys)).map((s) =>
+  //     s ? deserializeMint(s.data) : null
+  //   )
+  // );
 
-  swapInfos.forEach((swap) => {
-    const token0 = TOKEN_LIST.find(
-      (tok) => tok.address === swap.token0.mint.toString()
-    );
-    const token1 = TOKEN_LIST.find(
-      (tok) => tok.address === swap.token1.mint.toString()
-    );
+  // const accounts: { coingeckoId: string; account: PublicKey }[] = [];
+  // const unknownAccounts: { mint: PublicKey; account: PublicKey }[] = [];
 
-    pushToken({
-      coingeckoId: token0?.extensions?.coingeckoId,
-      mint: swap.token0.mint,
-      reserves: swap.token0.reserves,
-    });
-    pushToken({
-      coingeckoId: token1?.extensions?.coingeckoId,
-      mint: swap.token1.mint,
-      reserves: swap.token1.reserves,
-    });
-  });
+  // const pushToken = ({
+  //   coingeckoId,
+  //   mint,
+  //   reserves,
+  // }: {
+  //   coingeckoId?: string;
+  //   mint: PublicKey;
+  //   reserves: PublicKey;
+  // }) => {
+  //   if (!coingeckoId) {
+  //     // treat CASH as USDC for accounting purposes
+  //     if (mint.toString() === "CASHVDm2wsJXfhj6VWxb7GiMdoLc17Du7paH4bNr5woT") {
+  //       coingeckoId = "usd-coin";
+  //     }
+  //   }
+  //   if (coingeckoId) {
+  //     accounts.push({
+  //       coingeckoId,
+  //       account: reserves,
+  //     });
+  //   } else {
+  //     unknownAccounts.push({
+  //       mint,
+  //       account: reserves,
+  //     });
+  //   }
+  // };
 
-  await fs.mkdir("data/", { recursive: true });
-  await fs.writeFile(
-    "data/swaps.json",
-    JSON.stringify(
-      swapInfos
-        .map((s) => {
-          const decimals = mints.find((m) => m[0]?.equals(s.poolMint))?.[1]
-            ?.decimals;
-          if (typeof decimals !== "number") {
-            throw new Error(
-              `No decimals found for mint ${s.poolMint.toString()}`
-            );
-          }
-          return {
-            lp: s.poolMint.toString(),
-            reserve0: s.token0.reserves.toString(),
-            reserve1: s.token1.reserves.toString(),
-            token0: s.token0.mint.toString(),
-            token1: s.token1.mint.toString(),
-            decimals,
-          };
-        })
-        .sort((a, b) => (a.lp < b.lp ? -1 : 1)),
-      null,
-      2
-    )
-  );
-  await fs.writeFile(
-    "data/known-accounts.json",
-    JSON.stringify(
-      accounts.map((a) => ({ ...a, account: a.account.toString() })),
-      null,
-      2
-    )
-  );
-  await fs.writeFile(
-    "data/unknown-accounts.json",
-    JSON.stringify(
-      unknownAccounts.map((a) => ({
-        ...a,
-        account: a.account.toString(),
-        mint: a.mint.toString(),
-      })),
-      null,
-      2
-    )
-  );
-  console.log(
-    `Discovered and wrote ${accounts.length} known token reserves, ${unknownAccounts.length} unknown reserves.`
-  );
+  // swapInfos.forEach((swap) => {
+  //   const token0 = TOKEN_LIST.find(
+  //     (tok) => tok.address === swap.token0.mint.toString()
+  //   );
+  //   const token1 = TOKEN_LIST.find(
+  //     (tok) => tok.address === swap.token1.mint.toString()
+  //   );
+
+  //   pushToken({
+  //     coingeckoId: token0?.extensions?.coingeckoId,
+  //     mint: swap.token0.mint,
+  //     reserves: swap.token0.reserves,
+  //   });
+  //   pushToken({
+  //     coingeckoId: token1?.extensions?.coingeckoId,
+  //     mint: swap.token1.mint,
+  //     reserves: swap.token1.reserves,
+  //   });
+  // });
+
+  // await fs.mkdir("data/", { recursive: true });
+  // await fs.writeFile(
+  //   "data/swaps.json",
+  //   JSON.stringify(
+  //     swapInfos
+  //       .map((s) => {
+  //         const decimals = mints.find((m) => m[0]?.equals(s.poolMint))?.[1]
+  //           ?.decimals;
+  //         if (typeof decimals !== "number") {
+  //           throw new Error(
+  //             `No decimals found for mint ${s.poolMint.toString()}`
+  //           );
+  //         }
+  //         return {
+  //           lp: s.poolMint.toString(),
+  //           reserve0: s.token0.reserves.toString(),
+  //           reserve1: s.token1.reserves.toString(),
+  //           token0: s.token0.mint.toString(),
+  //           token1: s.token1.mint.toString(),
+  //           decimals,
+  //         };
+  //       })
+  //       .sort((a, b) => (a.lp < b.lp ? -1 : 1)),
+  //     null,
+  //     2
+  //   )
+  // );
+  // await fs.writeFile(
+  //   "data/known-accounts.json",
+  //   JSON.stringify(
+  //     accounts.map((a) => ({ ...a, account: a.account.toString() })),
+  //     null,
+  //     2
+  //   )
+  // );
+  // await fs.writeFile(
+  //   "data/unknown-accounts.json",
+  //   JSON.stringify(
+  //     unknownAccounts.map((a) => ({
+  //       ...a,
+  //       account: a.account.toString(),
+  //       mint: a.mint.toString(),
+  //     })),
+  //     null,
+  //     2
+  //   )
+  // );
+  // console.log(
+  //   `Discovered and wrote ${accounts.length} known token reserves, ${unknownAccounts.length} unknown reserves.`
+  // );
 };
 
 fetchAllSwaps().catch((err) => {
